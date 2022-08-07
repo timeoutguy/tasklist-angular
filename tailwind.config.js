4b72a7f75a8ccb73d94a7a509b7beef66baccd44
Marcus Vinícius <marcus.dev@protonmail.com>
2022-08-06 22:51:06 -0300

    Dependencias e configuração do tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      'main-lighter': '#1C1B2C',
+      'main-darker': '#0F0E17',
+      'text': '#A7A9BE',
+      'secondary': '#FF8906',
+      'card': '#353150',
+      'white': '#FFFFFF',
+      'red': colors.red,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
